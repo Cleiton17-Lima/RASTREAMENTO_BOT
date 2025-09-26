9f391e802ab870fee955c4c3d431c6a52ac4759c
CLEITON SANTOS LIMA <cleiton_dgk02@outlook.com>
2025-09-26 15:44:30 -0300

    Update zap.py

--- services/zap.py
+++ services/zap.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 INSTANCE = os.getenv("ZAPI_INSTANCE")
 TOKEN = os.getenv("ZAPI_TOKEN")
+DEFAULT_PHONE = os.getenv("ZAPI_PHONE")
 
-def enviar_mensagem(numero: str, texto: str):
+def enviar_mensagem(numero: str = DEFAULT_PHONE, texto: str = "🚚 Teste de rastreio"):
     url = f"https://api.z-api.io/instances/{INSTANCE}/token/{TOKEN}/send-text"
-
 
     payload = {
         "phone": numero,
@@ -17,6 +17,8 @@ def enviar_mensagem(numero: str, texto: str):
     }
 
     try:
-        requests.post(url, json=payload)
+        response = requests.post(url, json=payload)
+        print("Status:", response.status_code)
+        print("Resposta:", response.text)
     except Exception as e:
         print("Erro ao enviar mensagem Z-API:", e)
